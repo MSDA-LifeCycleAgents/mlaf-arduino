24e4a75f4633578de00cae7a4c09a83ecbae6d87
Rogier Pijpers <rogierpijpers1992@gmail.com>
2018-01-20 23:36:19 +0100

    Added missing file ExampleSensorAgent

---
+++ MLAF_boot/ExampleSensorAgent.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "sensor_agent.h"
+#include "temp_sensor.h"
+#include "accelero_sensor.h"
+#include "arduino_adc_sensor.h"
+
+class ExampleSensorAgent : public SensorAgent{
+  public:
+    ExampleSensorAgent(const char* name, NTPClient& ntp, int port) : SensorAgent(name, ntp, port){}
+
+    void setup(){
+      addSensor(new TempSensor());
+      addSensor(new AcceleroSensor());
+      addSensor(new ADCSensor(A0));
+
+      // optionals:
+      setIdentifier("someidentifier");
+      setTopic("sometopic");
+      
+      addBehaviour([this] {    
+          AclMessage* message = receive();
+
+          if (message) {
+              Serial.println("Received: " + message->toString());
+
+              AclMessage* response = message->createReply(INFORM);
+              response->content = "I received your message!";
+
+              send(response);
+              AclMessage::destroy(message);
+            }
+        });
+    }
+};
+
